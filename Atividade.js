731ad2307eb82ad5d6e924d9efbaf8f6463f332f
Pedro Fernandes <61945845+phcaf@users.noreply.github.com>
2021-06-14 13:20:23 -0300

    Segundo Quesito

--- Atividade.js
+++ Atividade.js
@@ -4,4 +4,21 @@ function quesitoUm(text)
 console.log(`OLÁ ${text} !`)
 }
 
+//Questão2
+function quesitoDois(valorUm,valorDois,callback) 
+{
+console.log(callback(valorUm,valorDois));
+}
+quesitoDois(10,12, function(valorUm,valorDois){
+    return valorUm + valorDois;
+}) 
+quesitoDois(14,25, function(valorUm,valorDois){
+    return valorUm - valorDois;
+}) 
+quesitoDois(20,40, function(valorUm,valorDois){
+    return valorUm / valorDois;
+}) 
+quesitoDois(2,35, function(valorUm,valorDois){
+    return valorUm * valorDois;
+}) 
 

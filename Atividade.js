92bed0ba63a24a3aa7bcf04d74f4c323b22271a6
Pedro Fernandes <61945845+phcaf@users.noreply.github.com>
2021-06-14 13:55:25 -0300

    Quarto quesito

--- Atividade.js
+++ Atividade.js
@@ -32,3 +32,50 @@ var valoresNumeros = verificaValor(10, function (a, b)
     a % b == 0 ? true : false;
 });
 
+//Questão4
+
+function verificaMes(c)
+{
+    switch (c)
+    {
+        case 1:
+            return 'Janeiro';
+            break;
+        case 2:
+            return 'Fevereiro';
+            break;
+        case 3:
+            return 'Março';
+            break;
+        case 4:
+            return 'Abril';
+            break;
+        case 5: 
+            return 'Maio';
+            break;
+        case 6:
+            return 'Junho';
+            break;
+        case 7:
+            return 'Julho';
+            break;
+        case 8:
+            return 'Agosto';
+            break;
+        case 9:
+            return 'Setembro';
+            break;
+        case 10:
+            return 'Outubro';
+            break;
+        case 11:
+            return 'Novembro';
+            break;
+        case 12:
+            return 'Dezembro';
+            break;
+    }
+}
+
+
+
